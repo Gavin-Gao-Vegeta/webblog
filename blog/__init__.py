0796bdce2a002d0c0e9a7c6a781b6ca492b0f113
Yuan Gao <gaoy78@cardiff.ac.uk>
2021-02-05 01:21:35 +0800

    Initial commit

---
+++ blog/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '653e3726979054fb94f323eec726295cab98de938d8db548'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c1993662:Yali1136!Yali1136!@csmysql.cs.cf.ac.uk:3306/c1993662_Blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:123456@127.0.0.1:3306/blog'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# app.config.from_object('config')
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+from blog import routes
+
+from flask_admin import Admin
+from blog.views import AdminView
+from blog.models import User, Post, Comment
+admin = Admin(app,name='Admin panel',template_mode='bootstrap3')
+admin.add_view(AdminView(User, db.session))
+admin.add_view(AdminView(Post, db.session))
+admin.add_view(AdminView(Comment, db.session))
